7a1f97254ded48df9dff61ad881f7fd0d609852c
Marc-Desktop <marcamillian@gmail.com>
2013-02-09 18:04:37 +0000

    Set up a new player object

--- Classes/GameObject.js
+++ Classes/GameObject.js
@@ -1,68 +1,96 @@
 var GameObject = function(){
 	
+	
+	
 }
 
 GameObject.prototype = function(){
 	
-	// IMAGE
-	var image;
+	// IMAGE ===================================================
 	
-	// MOVEMENT
-	var posX;
-	var posY;
-	var height = 50;
-	var width = 50;
-	
-	// ANIMATION
-	var animation;
-	var frame;
-	var frameTime;
-	var facing; // 0 == right way round , 1 == flipped
+	this.image;
 	
 	
-	var flip = new Array(	function(ctx){
+	
+	// MOVEMENT ================================================
+	
+	this.posX;
+	this.posY;
+	this.height = 50;
+	this.width = 50;
+	
+	
+	
+	// ANIMATION ================================================
+	
+	this.animation;
+	this.frame;
+	this.frameTime;
+	this.facing; // 0 == right way round , 1 == flipped
+	
+	
+	this.flip = new Array(	function(ctx){
 							},
 							function(ctx){	ctx.scale(-1,1);
 							} 
 						);
-	var animationData = new Array;
+	this.animationData = new Array;
 	// each animation has a new Array ( Frame Rate [ms] , #Frames, Looping?)
-	animationData.push(new Array(300, 1, true));
+	this.animationData.push(new Array(300, 1, true));
 	
-	setUp();
 	
-	setUp = function(){
-		posX = 20;
-		posY = 20;
+	
+	
+	// ============================================================
+	
+	this.setUp = function(){
+		this.posX = 20;
+		this.posY = 20;
 		
-		animation = 0;
-		frame = 0;
-		frameTime = 0;
-		facing = 0;
-		
-	}
-	
-	update = function(){
-		
-	}
-	
-	draw = function(){
+		this.animation = 0;
+		this.frame = 0;
+		this.frameTime = 0;
+		this.facing = 0;
 		
 	}
 	
-	isVisible = function(){
+	this.update = function(){
 		
 	}
 	
-	animate = function(){
+	this.draw = function(ctx){
+		var relPosX = posX; // - view ?
+		var relPosY = posY;
+		
+		ctx.save();
+		ctx.translate(relPosX, relPosY);
+		flip[facing](ctx);
+		ctx.drawImage(image,	// source
+							frame*width, animation*height, width, height,
+								// destination
+							-width/2, -height/2, width, height);
+		ctx.restore();
+	}
+	
+	this.isVisible = function(){
+		
+	}
+	
+	this.animate = function(){
 		// fill in the stuff here
+	}
+	
+	this.speak = function(){
+		
+		alert("say soemthing dummy");
 	}
 	
 	return{
 		setUp:setUp,
 		update:update,
 		draw:draw,
-		isVisible:isVisible
+		isVisible:isVisible,
+		speak:speak,
 	}
 	
 }();
--- Engine.js
+++ Engine.js
@@ -7,6 +7,13 @@ function object(o){
 }
 */
 
+// STANDARD FUNCTIONS
+function create(parent){
+	var F = function(){};	// create an empty function
+	F.prototype = parent;	// set the parent prototype
+	return new F();			// give back this object with the right prototype 
+}
+
 // IMPORT IMAGES
 
 var charSprite = new Image();
@@ -20,7 +27,9 @@ var init = function(){
 	var engine1 = new GameEngine();
 	engine1.setUp();
 	
-	var player = new GameObject(image, 20, 20);
+	var player1 = new GameObject;
+	
+	player1.speak();
 	
 	return setInterval(engine1.update, 33);
 	
