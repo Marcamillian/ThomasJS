dc4a289b3689000ee46b142bf9d28695abe1a76e
Marc-Laptop <marcamillian@gmail.com>
2013-02-18 17:51:51 +0000

    Game Camera Addition

---
+++ Classes/GameCamera.js
@@ -0,0 +1,23 @@
+var GameCamera = function(canvas){
+	
+	var posX = 0;
+	var posY = 0;
+	
+	var WIDTH = canvas.width;
+	var HEIGHT = canvas.height;
+	
+	
+	
+}
+
+GameCamera.prototype = function(){
+	
+	this.drawView = function(ctx, objects){
+		// accesses each object and draws it if its in the cameras view
+		
+		
+	}
+	
+	
+	
+}();
