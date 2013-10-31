348340cc8c63ef2d310475740ad9e5f5e34d4fa5
Marc-Laptop <marcamillian@gmail.com>
2013-10-31 14:18:19 +0000

    getAnim working for 1 animation
    next - pull all animations for a sprite

--- ThomasJS.js
+++ ThomasJS.js
@@ -38,37 +38,13 @@ var ThomasJS = {
 		$.getJSON('Assets/animationData.xml',"something", function(data) {
 			
 			ThomasJS.animationValues = data.animations;
-				
-			// sprite group loop 
-			for( var i=0 ; i < data.animations.length ; i++){
-			
-				// the sprite name
-				var spriteKey = Object.keys(data.animations[i]);
-				//alert("spriteKey :" + spriteKey);
-				
-				// anim loop
-				for( var j=0 ; j < data.animations[i][spriteKey[0]].length ; j++){
-					var animKey = Object.keys(data.animations[i][ spriteKey[0] ][j]);
-					//alert("animKey : " + animKey);
-					
-					animValues = data.animations[i][ spriteKey[0] ][j][ animKey[0] ];
-					
-					/*
-					ThomasJS.AnimationValues[spriteKey][animKey]["framerate"] = animValues[0]['framerate'];
-					ThomasJS.AnimationValues[spriteKey][animKey]["frames"] = animValues[0]['frames'];
-					ThomasJS.AnimationValues[spriteKey][animKey]["loop"] = animValues[0]['loop'];
-					*/
-				}
-				
-			}
-			
-			ThomasJS.getAnims("player", "walk");
+
+			alert(ThomasJS.getAnims("player", "walk"));
 			
 		});
    		
 		
 		// initalise the gameObjects
-		
 		
 		var background = Object.create(GameObject.prototype);
 		background.setup(this.backgroundSprite, [ 0, 0, 2400, 728] , 'red');
@@ -82,47 +58,6 @@ var ThomasJS = {
 		var wMachine = Object.create(GameObject.prototype);
 		wMachine.setup(this.wMachineSprite, [0, 0, 100, 100], 'red');
 		this.objManager.addObject(wMachine);
-		
-		/*
-		var midPoint = Object.create(GameObject.prototype);
-		midPoint.setup(null, [0,0, 20, 20], 'green');
-		this.objManager.addObject(midPoint);
-		
-		
-		var tr = Object.create(GameObject.prototype);
-		tr.setup(null,[50,-50, 40, 40], 'blue');
-		this.objManager.addObject(tr);
-		
-		var tl = Object.create(GameObject.prototype);
-		tl.setup(null,[-50,-50, 40, 40], 'red');
-		this.objManager.addObject(tl);
-		
-		var br = Object.create(GameObject.prototype);
-		br.setup(null,[50,50, 40, 40], 'yellow');
-		this.objManager.addObject(br);
-		
-		var bl = Object.create(GameObject.prototype);
-		bl.setup(null, [-50,50, 40, 40], 'pink');
-		this.objManager.addObject(bl);
-		
-		
-		
-		var tr2 = Object.create(GameObject.prototype);
-		tr2.setup(null,[100,-100, 20, 20], 'blue');
-		this.objManager.addObject(tr2);
-		
-		var tl2 = Object.create(GameObject.prototype);
-		tl2.setup(null,[-100,-100, 20, 20], 'red');
-		this.objManager.addObject(tl2);
-		
-		var br2 = Object.create(GameObject.prototype);
-		br2.setup(null, [100,100, 20, 20], 'yellow');
-		this.objManager.addObject(br2);
-		
-		var bl2 = Object.create(GameObject.prototype);
-		bl2.setup(null, [-100,100, 20, 20], 'pink');
-		this.objManager.addObject(bl2);
-		*/
 		
 		// CREATE THE INPUT MANAGER
 		this.inputManager = Object.create(InputManager.prototype);
@@ -147,10 +82,6 @@ var ThomasJS = {
 		var viewObjects = ThomasJS.objManager.getObjects(ThomasJS.camera.dims);
 		ThomasJS.camera.draw(ThomasJS.ctx, viewObjects);
 		
-		/*
-		var viewObjects = ThomasJS.objManager.update();
-		ThomasJS.camera.draw(ThomasJS.ctx, viewObjects);
-		*/
 	},
 	
 	advGameTime: function(){
@@ -164,37 +95,39 @@ var ThomasJS = {
 		
 	},
 	
-	getAnims: function(sprite, anim){
+	getAnims: function(_spriteSearch, _animSearch){
 		
-		var animData = ThomasJS.animationValues; // store the data in this closure
+		var animData = ThomasJS.animationValues; 
 		
-		// need to be able to get te index of the sprite / anim
-		var spriteKeys = new Array(animData.length); // used 'new Array' so that you get ['',''] rather than [2]
-		alert("sprites : " + spriteKeys.length);
+		var spriteIndex = null;
+		var animIndex = null;
 		
-		
+		//search for sprite index
 		for( var i=0 ; i < animData.length ; i++){
-			
-				// the sprite name
-				spriteKeys[i] = Object.keys(animData[i]);
-				alert("our sprite Keys : " + spriteKeys);
-				//spriteKeys[i] = new Array(animData[i][ spriteKeys[i] ].length);
+			if ( Object.keys(animData[i]) == _spriteSearch ){
+				spriteIndex = i;
 				
-				// anim loop
-				for( var j=0 ; j < animData[i][ spriteKeys[i] ].length ; j++){
-					
-					//var spriteKeys[i][j] = Object.keys( animData[i][ spriteKeys[i] ][j] );
-					alert( spriteKeys[i] + " : "  + Object.keys( animData[i][ spriteKeys[i] ][j] ));
+				for ( var j=0; j < animData[spriteIndex][ _spriteSearch ].length; j++){
+					if ( Object.keys( animData[spriteIndex][_spriteSearch][j] ) == _animSearch){
+						animIndex = j;
+						break;
+					}
 					
 				}
 				
+				break;
+				
+			} else{
+				alert("sprite not found");
+			}
 		}
 		
-		//alert("something : " + spriteKeys[0][0]);
-			
-		//alert(spriteKeys[0][0]);
+		var searchReturn = animData[spriteIndex][_spriteSearch][animIndex][_animSearch];
 		
-		//alert(animData[0][ sprite ][0][ anim ][0]["framerate"]);
+		var returnValues = [ searchReturn[0]['framerate'], searchReturn[1]['frames'], searchReturn[2]['loop'] ];
+		
+		return returnValues;
+		
 	}
 }
 
